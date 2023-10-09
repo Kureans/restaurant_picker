ae7489c97fe540a1ff122d07c2bdb6e53962d342
Kureans <leonardtengys@gmail.com>
2023-10-09 16:56:14 +0800

    Add endpoints for adding restaurant and getting a random restaurant

--- server/dist/index.js
+++ server/dist/index.js
@@ -7,10 +7,28 @@ const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
-const port = process.env.PORT || 8080;
-app.get('/', (req, res) => {
-    res.send('Express + TypeScript Server');
+const port = process.env.PORT || 8000;
+const restaurantList = [];
+app.use(express_1.default.json());
+app.post('/restaurant', (req, res) => {
+    const restaurant = req.body.restaurant;
+    if (!restaurant) {
+        return res.status(400).json({ error: "restaurant name is missing!" });
+    }
+    restaurantList.push(restaurant);
+    res.status(201).send();
+});
+app.get('/restaurant', (req, res) => {
+    if (restaurantList.length == 0) {
+        return res.json({
+            "error": "No restaurants submitted yet!"
+        });
+    }
+    const index = Math.floor(Math.random() * (restaurantList.length - 1));
+    return res.json({
+        "restaurant": restaurantList[index]
+    });
 });
 app.listen(port, () => {
-    console.log(`[server]: Server is running at http://localhost:${port}`);
+    console.log(`Server is running at http://localhost:${port}`);
 });
--- server/index.ts
+++ server/index.ts
@@ -4,12 +4,32 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8000;
+const restaurantList: Array<string> = [];
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
+app.use(express.json());
+
+app.post('/restaurant', (req: Request, res: Response) => {
+    const restaurant = req.body.restaurant;
+    if (!restaurant) {
+        return res.status(400).json({error: "restaurant name is missing!"});
+    }
+    restaurantList.push(restaurant);
+    res.status(201).send();
 });
 
+app.get('/restaurant', (req: Request, res: Response) => {
+    if (restaurantList.length == 0) {
+        return res.json({
+            "error": "No restaurants submitted yet!"
+        });
+    }
+    const index = Math.floor(Math.random() * (restaurantList.length-1));
+    return res.json({
+        "restaurant": restaurantList[index]
+    });
+})
+
 app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
